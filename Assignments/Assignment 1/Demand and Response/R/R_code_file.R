69a9a633b9d5d7c3900bc1668f4d9c2dc5f0f7a4
Christopher Alexander Trotter <chrisat@ifi.uio.no>
2018-02-08 12:41:13 +0100

    Question 1 - Updated the report and attempted to do it in R(unsuccessfully)

--- Assignments/Assignment 1/Demand and Response/R/R_code_file.R
+++ Assignments/Assignment 1/Demand and Response/R/R_code_file.R
@@ -1,19 +1,15 @@
-library(readr)
-library(downloader)
 
-# directory <- 'http://api.bitcoincharts.com/v1/csv/'
-# filename <- 'coinbaseUSD.csv.gz'
+# Question 1
+# What strategy to adopt.
 
-#directory <- 'C:\\Skole, Programmering\\Data Science\\Bitcoin Data\\Dataset\\'
-#filename <- 'coinbaseUSD.csv'
-#filename <- 'sample.csv'
+peak_hour <- c(5:8)
+time_frame <- c(1:24)
+peak_price <- 1
+off_peal_price <- 0.5
 
-grabRemote <- function( directory, filename, bitcoinData ) {
-  temp <- tempfile()
-  download.file( paste( directory, filename, sep = "" ), destfile = temp )
-  bitcoinData <- read.csv( gzfile( temp ), nrows = 1000 )
-  unlink( temp )
-  return( bitcoinData )
-}
+appliances_names <- c("Washing machine", "Electrical Vehicle", "Diswasher")
+appliances_costs <- c(1.94, 9.9, 1.44)
+appliances <- data.frame( appliances_names, appliances_costs )
+names( appliances ) <- c( "Appliances", "Energy cost" )
 
-print("stupid potato")
+daily_rates <- data.frame()
