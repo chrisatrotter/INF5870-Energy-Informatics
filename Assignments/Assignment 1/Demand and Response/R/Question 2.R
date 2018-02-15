26cfd0bb07e507375b70f3eb8b6d6f556eecb7a4
Khalil <khalil_abuawad@hotmail.com>
2018-02-15 14:07:22 +0100

    runtimes and costs added

--- Assignments/Assignment 1/Demand and Response/R/Question 2.R
+++ Assignments/Assignment 1/Demand and Response/R/Question 2.R
@@ -10,12 +10,17 @@ daily_rates <- data.frame( time_frame )
 daily_rates$cost <- mapply( daily_rates$time_frame, FUN=function(x) if( x %in% peak_hour ) peak_price else off_peak_price )
 names( daily_rates ) <- c( "time", "cost" )
 
+#appliances name
 appliances_names_random <- c("coffee maker", "ceiling fan", "hair dryer", "toaster", "microwave", "router", "cellphone charger", "cloth iron")
 appliances_names_shiftable <- c("Washing machine", "Electrical Vehicle", "Diswasher", "dryer")
 appliances_names_nonshiftable <- c("Lighting", "Heating", "Refrigerator-freezer", "Electric stove", "TV","Computer")
+
+#appliances costs
 appliances_costs_shiftable <- c(1.94, 9.9, 1.44, 2.5)
 appliances_costs_nonshiftable <- c(runif(1, min=1, max=2), runif(1, min=6.4, max=9.6), runif(1, min=1.32, max=3.96), 3.9, runif(1, min=0.15, max=0.6), 0.6)
 appliances_costs_random <- c(0.08, 0.0075, 0.0250, 0.0240, 0.0600, 0.0006, 0.0005, 0.0275)
+
+#appliances run time
 appliances_runtime_random <- c(1,3,1,1,1,24,3,1)
 appliances_runtime_shiftable <- c(1, 4, 1, 1)
 appliances <- data.frame( appliances_names, appliances_costs )
