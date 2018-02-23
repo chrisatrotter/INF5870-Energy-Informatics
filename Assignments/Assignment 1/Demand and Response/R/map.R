008cd6bfe758a4b0b72765769a924ca12e4e2ba5
puffmonkey <trotters.christopher@gmail.com>
2018-02-23 03:45:00 +0100

    Random_appliances - edited to same format as appliances.csv

--- Assignments/Assignment 1/Demand and Response/R/map.R
+++ Assignments/Assignment 1/Demand and Response/R/map.R
@@ -1,48 +1,37 @@
 # Question 2
 
+library(lpSolveAPI)
+
 setwd(getwd())
 directory <- 'data/'
-appliance_file <- 'appliances.csv'
-random_appliance_file <- 'random-appliances.csv'
-time_frame <- c(1:24)
-peak_hour <- c(17:20)
+appliances_file <- 'appliances.csv'
+random_appliances_file <- 'random_appliances.csv'
+time.frame <- c(1:24)
+peak.hour <- c(17:20)
 
-RTP <- c(runif(16, min=0.1, max=0.5), runif(4, min=0.5, max=1.0), runif(4, min=0.1, max=0.5))
+rtp <- c(runif(16, min=0.1, max=0.5), runif(4, min=0.5, max=1.0), runif(4, min=0.1, max=0.5))
 
-#RTP
+appliances <- read.csv( paste(directory, appliances_file, sep="") )
 
-appliances <- read.csv( paste(directory, appliance_file, sep="") )
-#appliances$timespan <- (function(x) x)(appliances[appliances$Appliances,])
-#within(appliances, {timespan <- Map(Earliest, Latest, list); appliances})
+random.appliances <- read.csv( paste(directory, random_appliances_file, sep="") )
 
-#appliances$timespan <- mapply(list, appliances$Earliest, appliances$Latest)
-#appliances
-
-#earliest <- list(appliances$Earliest)
-#latest <- list(appliances$Latest)
-
-#appliances <- map(earliest, latest, list)
-
-#timespan <- with(appliances, seq(earliest, latest, 1))
-#appliances$timespan <- (function(x, y) list(x:y))(appliances$Earliest, appliances$Latest)
-#timespan
-#appliances$timespan <- list(appliances[appliances$Appliances,4]:appliances[appliances$Appliances,5])
-
-#appliances$timespan <- with(appliances, Earliest+Latest)
-#appliances
+appliances <- rbind(appliances, random.appliances[sample(1:nrow(random.appliances), round(runif(1, min=1, max=nrow(random.appliances))), replace = FALSE),])
 
 appliances$timespan <- Map(`:`, appliances$Earliest, appliances$Latest)
+
+daily.rates <- data.frame( time.frame, rtp )
+names( daily.rates ) <- c( "time", "cost" )
+
+# Number of appliances
+appliances.length <- length(appliances$Appliances)
+
+f.obj = rep(daily.rates$cost, appliances.length)
+
 appliances
+number.of.constraints <- sum(sapply(appliances$timespan, function(x){sum(length(x))}))
+number.of.constraints
 
 
-random_appliance <- read.csv( paste(directory, random_appliance_file, sep="") )
-appliances <- rbind(appliances, random_appliance[sample(1:nrow(random_appliance), round(runif(1, min=1, max=nrow(random_appliance))), replace = FALSE),])
-appliances
-
-daily_rates <- data.frame( time_frame, RTP )
-names( daily_rates ) <- c( "time", "cost" )
-daily_rates
 
 
-p <- rep(daily_rates$cost, length(appliances$Appliances))
 
