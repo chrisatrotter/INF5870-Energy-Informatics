d343b4b398819f7af6a44fcc63fe9c7edf249c92
Christopher Alexander Trotter <chrisat@ifi.uio.no>
2018-04-19 17:17:37 +0200

    read in the different data files.

---
+++ Assignments/Assignment 2/Machine learning/Weather Forecast/R/Assignment_1.R
@@ -0,0 +1,7 @@
+# Read in data set with applliances
+setwd(getwd())
+directory <- 'data/'
+solution_data_file <- 'Solution.csv'
+training_data_file <- 'TrainData.csv'
+weather_forecast_input_file <- 'WeatherForecastInput.csv'
+
