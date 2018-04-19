ec9a35061516094baf7643c7bdebfc8b1d0528ad
Christopher Alexander Trotter <chrisat@ifi.uio.no>
2018-04-19 17:25:16 +0200

    Data read in to the project

--- Assignments/Assignment 2/Machine learning/Weather Forecast/R/Assignment_1.R
+++ Assignments/Assignment 2/Machine learning/Weather Forecast/R/Assignment_1.R
@@ -5,3 +5,7 @@ solution_data_file <- 'Solution.csv'
 training_data_file <- 'TrainData.csv'
 weather_forecast_input_file <- 'WeatherForecastInput.csv'
 
+# Read in data set with applliances
+training_data <- read.csv( paste(directory, training_data_file, sep="") )
+solution_data <- read.csv( paste(directory, solution_data_file, sep="") )
+weather_forecast_input <- read.csv(paste(directory, weather_forecast_input_file, sep=""))
