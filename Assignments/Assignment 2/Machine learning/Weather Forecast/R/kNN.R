b2840968f3bc4c75ea019ab7be3df666be10a3fb
Christopher Alexander Trotter <chrisat@ifi.uio.no>
2018-05-03 18:26:49 +0200

    added

--- Assignments/Assignment 2/Machine learning/Weather Forecast/R/kNN.R
+++ Assignments/Assignment 2/Machine learning/Weather Forecast/R/kNN.R
@@ -9,3 +9,5 @@ weather_forecast_input_file <- 'WeatherForecastInput.csv'
 training_data <- read.csv( paste(directory, training_data_file, sep="") )
 solution_data <- read.csv( paste(directory, solution_data_file, sep="") )
 weather_forecast_input <- read.csv(paste(directory, weather_forecast_input_file, sep=""))
+
+knn_pred <- knn(training_data, weather_forecast_input, )
