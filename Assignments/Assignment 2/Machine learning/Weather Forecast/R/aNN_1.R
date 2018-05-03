831b771e448e2879681493aecbef971a2e58444f
khalila <khalila@student.matnat.uio.no>
2018-05-03 11:49:41 +0200

    ann

--- Assignments/Assignment 2/Machine learning/Weather Forecast/R/aNN_1.R
+++ Assignments/Assignment 2/Machine learning/Weather Forecast/R/aNN_1.R
@@ -3,24 +3,39 @@ library(neuralnet)
 
 # Read in data set with applliances
 setwd(getwd())
+
 directory <- '../data/'
 solution_data_file <- 'Solution.csv'
 training_data_file <- 'TrainData.csv'
 weather_forecast_input_file <- 'WeatherForecastInput.csv'
 
 # Read in data set with applliances
-training_data <- read.csv( paste(directory, training_data_file, sep="") )
-solution_data <- read.csv( paste(directory, solution_data_file, sep="") )
-testData <- read.csv(paste(directory, weather_forecast_input_file, sep=""))
-testData
+training_data <- read.csv( paste(directory, training_data_file, sep=""))
+solution_data <- read.csv( paste(directory, solution_data_file, sep=""))
+input <- read.csv(paste(directory, weather_forecast_input_file, sep=""))
 
-powerModel <- neuralnet(training_data$POWER ~ training_data$WS10, data = training_data, hidden=10)
+training_data_sample <- training_data[12001:15000,]
+testData <- training_data[15001:15720,]
+
+powerModel <- neuralnet(training_data_sample$POWER ~ training_data_sample$WS10, data = training_data_sample, hidden=10)
+
+## the length of weatherforecastinput is 720 not 336??? why
+## why doesnt weatherforecastinput have power variable?
+## what am i suppose to use were. 1
 
 # generate the prediction on the test data
-modelResults <- compute(powerModel, testData$WS10)
+modelResults <- compute(powerModel, input$WS10)
 powerPredictionNN <- modelResults$net.result
 
 # plot the neural networks
 plot(powerModel)
 
+rmse <- function(error)
+{
+  sqrt(mean(error^2))
+}
+
 # calculate error
+errorNN <- powerPredictionNN - testData$POWER
+
+rmse(errorNN)
