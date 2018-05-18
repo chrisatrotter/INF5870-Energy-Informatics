7c8a35ee7c59f7d54181e95e08b69d4bb1e81318
puffmonkey <trotters.christopher@gmail.com>
2018-05-18 22:02:07 +0200

    added model of RNN

--- Assignments/Assignment 2/Machine learning/Weather Forecast/R/RNN.R
+++ Assignments/Assignment 2/Machine learning/Weather Forecast/R/RNN.R
@@ -1,11 +1,12 @@
 #Installation of packages
-packages <- c("rnn")
+packages <- c("rnn", "caret")
 if (length(setdiff(packages, rownames(installed.packages())))){
   install.packages(setdiff(packages, rownames(installed.packages())))
 }
 
 # Loading of packages
 library(rnn)
+library(caret)
 
 # Read in data set with applliances
 setwd(getwd())
@@ -34,31 +35,60 @@ Power <- training_data$POWER
 set.seed(333)
 
 # Training-testing sets
-train <- 1:15360
-test <- 15361:1608
+train_data<- 14300:15360
+test <- 15361:16080
 
-X <- Power[sample(x = train, size = 1000)]
+#X <- Power[sample(x = train_data, size = 1000)]
+X <- Power[train_data]
 X <- array(X, dim=c(NROW(X), NCOL(X), 1))
 
-Y <- Power[sample(x = train, size = 1000)]
+#Y <- Power[sample(x = train_data, size = 1000)]
+Y <- Power[train_data]
 Y <- array(Y, dim=c(NROW(Y), NCOL(Y), 1))
 
 #model <- elman(Y = Y, X = X, size = 8, learnFuncParams=c(0.1), maxit = 100)
-model <- trainr(Y = Y, X = X, learningrate = 0.01, hidden_dim = 10, numepochs = 150)
+model_rnn <- trainr(Y = Y, X = X, learningrate = 0.01, hidden_dim = 10, numepochs = 150)
 
-Xpred <- Power[test]
-Xpred <- array(Xpred, dim = c(NROW(Xpred), NCOL(Xpred), 1))
+test_data <- array(Power[test], dim = c(NROW(Power[test]), NCOL(Power[test]), 1))
 
 # Predicted values
-Ypred <- predictr(model, Xpred)
-
-# Calculate error
-rmse(solution_data$POWER - Ypred)
-
+prediction_rnn <- predictr(model_rnn, test_data)
 
 # Training of the model (Supported Vector Regression)
-model_lr <- train(POWER ~ ., data = training_data[train,], method = "lm")
+model_lr <- train(POWER ~ POWER, data = train_data[1:15360,], method = "lm")
 
 # Predict new data by the trained model
-prediction_lr <- predict(model_lr, newdata = lr_testing)
+prediction_lr <- predict(model_lr, newdata = POWER[test])
+
+# Calculate LR error
+rmse(solution_data$POWER - prediction_lr)
+
+# Calculate RNN error
+rmse(solution_data$POWER - prediction_rnn)
+
+# Write results to file
+write.table(data.frame(weather_forecast_input$TIMESTAMP, prediction_lr),
+            paste(directory, forecast, forecast_model[6], sep=""),
+            sep=",",
+            col.names= c("TIMESTAMP", "FORECAST"),
+            row.names = F)
+
+# Plot predictions
+plot_prediction <- function(){
+  prediction_plot <- data.frame(predictionslr = prediction_lr,
+                                Ypred = prediction_rnn,
+                                powers = solution_data$POWER,
+                                month = as.POSIXct(solution_data$TIMESTAMP, format = "%Y%m%d %H:%M", origin = "1970-01-01"))
+  
+  ggplot(prediction_plot, aes(x = month)) +
+    geom_line(aes(y = powers), na.rm = TRUE, color = "grey27", size = 1, alpha=1) +
+    geom_line(aes(y = predictionslr), na.rm = TRUE, color = "blue", size = 1, alpha=1) +
+    geom_line(aes(y = Ypred), na.rm = TRUE, color = "red", size = 1, alpha=1) +
+    scale_x_datetime(date_breaks = "4 day", date_labels = "%b %d") +
+    xlab("November 2013") +
+    ylab("Power")
+}
+
+#plot_prediction()
+
 
